72d7feb7c4a1a741723a5d554efa36d6f6e08310
Freya Ard <ardfreya@gmail.com>
2024-10-10 15:26:10 -0600

    add support for wilcox feelings wheel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import "./App.css";
+import SelectWheelButton from "./components/SelectWheelButton";
 
 interface Emotion {
   text: string;
   subEmotions?: Emotion[];
 }
+export type WheelVersion = "wilcox" | "roberts";
 
-const EMOTIONS: Emotion[] = [
+const ROBERTS_EMOTIONS: Emotion[] = [
   {
     text: "Happy",
     subEmotions: [
@@ -209,18 +211,170 @@ const EMOTIONS: Emotion[] = [
   },
 ];
 
+const WILCOX_EMOTIONS: Emotion[] = [
+  {
+    text: "Peaceful",
+    subEmotions: [
+      {
+        text: "Content",
+        subEmotions: [{ text: "Pensive" }],
+      },
+      {
+        text: "Thoughtful",
+        subEmotions: [{ text: "Relaxed" }],
+      },
+      { text: "Intimate", subEmotions: [{ text: "Responsive" }] },
+      {
+        text: "Loving",
+        subEmotions: [{ text: "Serene" }],
+      },
+      {
+        text: "Trusting",
+        subEmotions: [{ text: "Sentimental" }],
+      },
+      { text: "Nurturing", subEmotions: [{ text: "Thankful" }] },
+    ],
+  },
+  {
+    text: "Powerful",
+    subEmotions: [
+      {
+        text: "Faithful",
+        subEmotions: [{ text: "Confident" }],
+      },
+      {
+        text: "Important",
+        subEmotions: [{ text: "Intelligent" }],
+      },
+      { text: "Hopeful", subEmotions: [{ text: "Worthwhile" }] },
+      {
+        text: "Respected",
+        subEmotions: [{ text: "Satisfied" }],
+      },
+      {
+        text: "Proud",
+        subEmotions: [{ text: "Cheerful" }],
+      },
+      { text: "Appreciated", subEmotions: [{ text: "Valuable" }] },
+    ],
+  },
+  {
+    text: "Joyful",
+    subEmotions: [
+      {
+        text: "Aware",
+        subEmotions: [{ text: "Delightful" }],
+      },
+      {
+        text: "Creative",
+        subEmotions: [{ text: "Extravagant" }],
+      },
+      { text: "Playful", subEmotions: [{ text: "Amused" }] },
+      {
+        text: "Energetic",
+        subEmotions: [{ text: "Stimulating" }],
+      },
+      {
+        text: "Vibrant",
+        subEmotions: [{ text: "Fascinating" }],
+      },
+      { text: "Excited", subEmotions: [{ text: "Daring" }] },
+    ],
+  },
+  {
+    text: "Scared",
+    subEmotions: [
+      {
+        text: "Rejected",
+        subEmotions: [{ text: "Bewildered" }],
+      },
+      {
+        text: "Confused",
+        subEmotions: [{ text: "Discouraged" }],
+      },
+      { text: "Helpless", subEmotions: [{ text: "Insignificant" }] },
+      {
+        text: "Submissive",
+        subEmotions: [{ text: "Weak" }],
+      },
+      {
+        text: "Insecure",
+        subEmotions: [{ text: "Foolish" }],
+      },
+      { text: "Anxious", subEmotions: [{ text: "Embarassed" }] },
+    ],
+  },
+  {
+    text: "Mad",
+    subEmotions: [
+      {
+        text: "Hurt",
+        subEmotions: [{ text: "Jealous" }],
+      },
+      {
+        text: "Hostile",
+        subEmotions: [{ text: "Selfish" }],
+      },
+      { text: "Angry", subEmotions: [{ text: "Frustrated" }] },
+      {
+        text: "Rage",
+        subEmotions: [{ text: "Furious" }],
+      },
+      {
+        text: "Hateful",
+        subEmotions: [{ text: "Irritated" }],
+      },
+      { text: "Critical", subEmotions: [{ text: "Skeptical" }] },
+    ],
+  },
+  {
+    text: "Sad",
+    subEmotions: [
+      {
+        text: "Guilty",
+        subEmotions: [{ text: "Bashful" }],
+      },
+      {
+        text: "Ashamed",
+        subEmotions: [{ text: "Stupid" }],
+      },
+      { text: "Depressed", subEmotions: [{ text: "Miserable" }] },
+      {
+        text: "Lonely",
+        subEmotions: [{ text: "Inadequate" }],
+      },
+      {
+        text: "Bored",
+        subEmotions: [{ text: "Inferior" }],
+      },
+      { text: "Sleepy", subEmotions: [{ text: "Apathetic" }] },
+    ],
+  },
+];
+
 function App() {
   const [selectedEmotion, setSelectedEmotion] = useState<Emotion | null>(null);
   const [previousEmotions, setPreviousEmotions] = useState<Array<Emotion>>([]);
+  const [wheelVersion, setWheelVersion] = useState<WheelVersion>("roberts");
+
+  const emotions =
+    wheelVersion === "roberts" ? ROBERTS_EMOTIONS : WILCOX_EMOTIONS;
 
   // if no emotion selected
   const isInitialState = !selectedEmotion;
 
-  // if emotion selected AND there ARE subEmotions
-  const isActiveState = !!selectedEmotion && !!selectedEmotion.subEmotions;
+  // if emotion selected AND there NO subEmotions OR there is ONLY ONE subEmotion
+  const isEndState =
+    !!selectedEmotion &&
+    (!selectedEmotion.subEmotions || selectedEmotion.subEmotions.length === 1);
 
-  // if emotion selected AND there NO subEmotions
-  const isEndState = !!selectedEmotion && !selectedEmotion.subEmotions;
+  // if emotion selected AND there ARE subEmotions
+  const isActiveState =
+    !!selectedEmotion && !!selectedEmotion.subEmotions && !isEndState;
+
+  if (selectedEmotion?.subEmotions?.length === 1) {
+    setSelectedEmotion(selectedEmotion.subEmotions[0]);
+  }
 
   const fadeAnimation = {
     initial: { opacity: 0 },
@@ -245,75 +399,85 @@ function App() {
   };
 
   return (
-    <motion.div
-      layout
-      className={`main ${previousEmotions[0]?.text.toLowerCase()}`}
-    >
-      {isInitialState || isActiveState ? (
-        <motion.h1 layout="position">How are you feeling today?</motion.h1>
-      ) : (
-        <h1>Are you feeling?</h1>
+    <>
+      {isInitialState && (
+        <motion.div layout style={{ display: "flex" }}>
+          <SelectWheelButton
+            wheelVersion={wheelVersion}
+            setWheelVersion={setWheelVersion}
+          />
+        </motion.div>
       )}
+      <motion.div
+        layout
+        className={`main ${previousEmotions[0]?.text.toLowerCase()}`}
+      >
+        {isInitialState || isActiveState ? (
+          <motion.h1 layout="position">How are you feeling today?</motion.h1>
+        ) : (
+          <h1>Are you feeling?</h1>
+        )}
 
-      {!isEndState && (
-        <ul>
-          {isInitialState &&
-            EMOTIONS.map((emotion) => {
-              return (
-                <motion.li>
-                  <motion.button
-                    {...fadeAnimation}
-                    onClick={() => {
-                      setSelectedEmotion(emotion);
-                      setPreviousEmotions([...previousEmotions, emotion]);
-                    }}
-                    className={emotion.text.toLowerCase()}
-                    key={emotion.text}
-                  >
-                    {emotion.text}
-                  </motion.button>
-                </motion.li>
-              );
-            })}
+        {!isEndState && (
+          <ul>
+            {isInitialState &&
+              emotions.map((emotion) => {
+                return (
+                  <motion.li>
+                    <motion.button
+                      {...fadeAnimation}
+                      onClick={() => {
+                        setSelectedEmotion(emotion);
+                        setPreviousEmotions([...previousEmotions, emotion]);
+                      }}
+                      className={emotion.text.toLowerCase()}
+                      key={emotion.text}
+                    >
+                      {emotion.text}
+                    </motion.button>
+                  </motion.li>
+                );
+              })}
 
-          {isActiveState &&
-            selectedEmotion.subEmotions?.map((emotion) => {
-              return (
-                <li>
-                  <motion.button
-                    {...fadeAnimation}
-                    onClick={() => {
-                      setSelectedEmotion(emotion);
-                      setPreviousEmotions([...previousEmotions, emotion]);
-                    }}
-                    className="shadow"
-                    key={emotion.text}
-                  >
-                    {emotion.text}
-                  </motion.button>
-                </li>
-              );
-            })}
-        </ul>
-      )}
+            {isActiveState &&
+              selectedEmotion.subEmotions?.map((emotion) => {
+                return (
+                  <li>
+                    <motion.button
+                      {...fadeAnimation}
+                      onClick={() => {
+                        setSelectedEmotion(emotion);
+                        setPreviousEmotions([...previousEmotions, emotion]);
+                      }}
+                      className="shadow"
+                      key={emotion.text}
+                    >
+                      {emotion.text}
+                    </motion.button>
+                  </li>
+                );
+              })}
+          </ul>
+        )}
 
-      {isActiveState && (
-        <button onClick={goBack} className="back-button">
-          Back
-        </button>
-      )}
-
-      {isEndState && (
-        <div className="end-state">
-          <motion.p {...fadeAnimation}>
-            {selectedEmotion.text.toUpperCase()}
-          </motion.p>
-          <button onClick={reset} className="back-button">
-            Start Over
+        {isActiveState && (
+          <button onClick={goBack} className="back-button">
+            Back
           </button>
-        </div>
-      )}
-    </motion.div>
+        )}
+
+        {isEndState && (
+          <div className="end-state">
+            <motion.p {...fadeAnimation}>
+              {selectedEmotion.text.toUpperCase()}
+            </motion.p>
+            <button onClick={reset} className="back-button">
+              Start Over
+            </button>
+          </div>
+        )}
+      </motion.div>
+    </>
   );
 }
 
---
+++ src/components/SelectWheelButton.tsx
@@ -0,0 +1,26 @@
+import { SetStateAction } from "react";
+import { WheelVersion } from "../App";
+
+interface SelectWheelButtonProps {
+  wheelVersion: WheelVersion;
+  setWheelVersion: React.Dispatch<SetStateAction<WheelVersion>>;
+}
+
+export default function SelectWheelButton({
+  wheelVersion,
+  setWheelVersion,
+}: SelectWheelButtonProps) {
+  const buttonPressed = () => {
+    if (wheelVersion === "roberts") setWheelVersion("wilcox");
+    if (wheelVersion === "wilcox") setWheelVersion("roberts");
+  };
+
+  let buttonText = "Extended";
+  if (wheelVersion === "wilcox") buttonText = "Original";
+
+  return (
+    <button onClick={buttonPressed} style={{ backgroundColor: "transparent" }}>
+      {buttonText}
+    </button>
+  );
+}
